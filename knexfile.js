44ee198774945ce981f80ac189f4b4cae18c0c73
yossef horvitz work pc <yossefhorvitz@gmail.com>
2025-06-15 19:06:56 +0300

    fix manual.yml

--- knexfile.js
+++ knexfile.js
@@ -26,22 +26,28 @@ const development = {
 
 const production = {
   client: 'pg',
-  connection: {
-    host: process.env.HOST,
-    port: process.env.DB_PORT,
-    database: process.env.DB_NAME,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    // connectionString: process.env.DATABASE_URL,
-    ssl: { rejectUnauthorized: false }
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
+    connection: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+  pool: { min: 0, max: 1 },
   migrations: {
     tableName: 'knex_migrations'
   }
+  // connection: {
+  //   host: process.env.HOST,
+  //   port: process.env.DB_PORT,
+  //   database: process.env.DB_NAME,
+  //   user: process.env.DB_USER,
+  //   password: process.env.DB_PASSWORD,
+  //   // connectionString: process.env.DATABASE_URL,
+  //   ssl: { rejectUnauthorized: false }
+  // },
+  // pool: {
+  //   min: 2,
+  //   max: 10
+  // },
+  // migrations: {
+  //   tableName: 'knex_migrations'
+  // }
 };
 
 export default {
