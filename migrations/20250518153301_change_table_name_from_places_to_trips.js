072e5755efb83d9e2e333a88bdc0d0da7604dea5
yossef horvitz <yossefhorvitz@gmail.com>
2025-06-17 14:45:11 +0300

    delete migration file

--- migrations/20250518153301_change_table_name_from_places_to_trips.js
+++
@@ -1,8 +0,0 @@
-export function up(knex) {
-    return knex.schema.renameTable('historic_places', 'historic_trips');
-  }
-  
-  export function down(knex) {
-    return knex.schema.renameTable('historic_trips', 'historic_places');
-  }
-  
